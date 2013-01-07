a491e5ff6286f9fd072cd48e4eddd1d144558ad9
Seth Woodworth <seth@sethish.com>
2013-01-07 15:45:18 -0500

    adding course_list homepage url

--- karmaworld/urls.py
+++ karmaworld/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import ListView
 
+from karmaworld.apps.courses.models import Course
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
@@ -11,4 +13,5 @@ urlpatterns = patterns('',
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', ListView.as_view(model=Course), name='home'),
 )
