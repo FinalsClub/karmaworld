8a03356d56d35b6590a7dff1141706f3fea0437d
Seth Woodworth <seth@sethish.com>
2013-01-17 15:09:21 -0500

    adding raw note iframe view, route and template

--- karmaworld/urls.py
+++ karmaworld/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import direct_to_template
 
 from karmaworld.apps.courses.models import Course
 from karmaworld.apps.courses.views import CourseDetailView
-from karmaworld.apps.notes.views import NoteDetailView
+from karmaworld.apps.notes.views import NoteDetailView, RawNoteDetailView
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
@@ -17,10 +17,15 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^about/$', direct_to_template, { 'template': 'about.html' }, name='about'), 
+    url(r'^about/$', direct_to_template, { 'template': 'about.html' }, name='about'),
 
-    url(r'^(?P<school_slug>[^/]+)/(?P<slug>[^/]+)$', CourseDetailView.as_view(), name='course_detail'),
-    url(r'^(?P<school_slug>[^/]+)/(?P<course_slug>[^/]+)/(?P<slug>[^/]+)$', NoteDetailView.as_view(), name='note_detail'),
+    # the raw route must come before routes with a capture group after the
+    # first / of the url
+    url(r'^raw/(?P<pk>\d+)$', RawNoteDetailView.as_view(), name='note_raw'),
+    url(r'^(?P<school_slug>[^/]+)/(?P<slug>[^/]+)$', \
+        CourseDetailView.as_view(), name='course_detail'),
+    url(r'^(?P<school_slug>[^/]+)/(?P<course_slug>[^/]+)/(?P<slug>[^/]+)$', \
+        NoteDetailView.as_view(), name='note_detail'),
 
     url(r'^$', ListView.as_view(model=Course), name='home'),
 )
