5369f47c3ed10caddf8bff36ea11dd889ff518f4
Seth Woodworth <seth@sethish.com>
2013-01-23 19:10:53 -0500

    removing additional context for adding NoteForm to course_detail template, not needed

--- karmaworld/apps/courses/views.py
+++ karmaworld/apps/courses/views.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
 # Copyright (C) 2012  FinalsClub Foundation
+
 from django.views.generic import DetailView
+
 from karmaworld.apps.courses.models import Course
 
 class CourseDetailView(DetailView):
     """ Class-based view for the course html page """
 
+    model = Course
     # name passed to template
     context_object_name = u"course"
-    model = Course
