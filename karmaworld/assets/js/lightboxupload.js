edb86ed04ea41d0f47ba038c83a04ed7cc19fecb
Seth Woodworth <seth@sethish.com>
2013-02-07 17:01:36 -0500

    moving qq.FileUploader to lightboxupload, #74

--- karmaworld/assets/js/lightboxupload.js
+++ karmaworld/assets/js/lightboxupload.js
@@ -8,4 +8,27 @@ $(function(){
   $(".lightbox_close").click(function() {
     $(".modal_content").hide();
   });
+
+  var uploader = new qq.FileUploader( {
+      action: uploadUrl, // added to page via template var
+      element: $('#file-uploader')[0],
+      multiple: true,
+      onComplete: function( id, fileName, responseJSON ) {
+        if( responseJSON.success )
+          console.log( "success!" ) ;
+          $('form#upload_form').attr('action', responseJSON.note_url);
+          $('input.submit_upload').show();
+      },
+      onAllComplete: function( uploads ) {
+        // uploads is an array of maps
+        // the maps look like this: { file: FileObject, response: JSONServerResponse }
+        console.log( "All complete!" ) ;
+      },
+      params: {
+        'csrf_token': csrf_token,
+        'csrf_name': 'csrfmiddlewaretoken',
+        'csrf_xname': 'X-CSRFToken',
+        'course_id': courseId, // added to page via template var
+      },
+    }) ;
 });
