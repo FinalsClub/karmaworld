d8e9d4b06bfdc14245091878480f17da51bf58f8
Seth Woodworth <seth@sethish.com>
2013-05-23 15:18:14 -0400

    comment the strip_html management command

--- karmaworld/apps/notes/management/commands/strip_html_to_text.py
+++ karmaworld/apps/notes/management/commands/strip_html_to_text.py
@@ -8,13 +8,17 @@ from django.core.management.base import BaseCommand
 from apps.notes.models import Note
 
 class Command(BaseCommand):
+    """ Command to process notes with html, and without text
+        to refill text sans html tags """
     args = 'none'
     help = "Take all notes with the .html property and use that to fill Note.text by stripping html"
 
     def handle(self, *args, **kwargs):
+        """ On all calls, clean all notes with html and not text using lxml """
         notes = Note.objects.filter(html__isnull=False).filter(text__isnull=True)
         cleaned_notes = 0
         for note in notes:
+            #TODO: find style tags and drop them and their contents first
             note.text = fromstring(note.html).text_content()
             note.save()
             cleaned_notes += 1
