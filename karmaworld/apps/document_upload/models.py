533a6e5ab2003b0aaf6048d38e957774e3edb62c
Seth Woodworth <seth@sethish.com>
2013-08-28 12:47:16 -0400

    refactor RawDocument model

--- karmaworld/apps/document_upload/models.py
+++ karmaworld/apps/document_upload/models.py
@@ -1,18 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# Copyright (C) 2013  FinalsClub Foundation
+
 import datetime
 
 from django.db import models
-
 import django_filepicker
 
-class IntakeDoc(models.Model):
-    fp_note = django_filepicker.models.FPFileField(upload_to='queue/%Y/%m/%j/',
-                help_text=u"An uploaded file reference from Filepicker.io")
-    form    = models.TextField(max_length=1024,
-                help_text=u"A serilized version of the note data form")
-    ip      = models.IPAddressField(blank=True, null=True,
-                help_text=u"IP address of the uploader")
+from karmaworld.apps.notes.models import Document
 
-    uploaded_at     = models.DateTimeField(null=True, default=datetime.datetime.utcnow)
+class RawDocument(Document):
+    is_processed = models.BooleanField(default=False)
+
 
     class Meta:
         """ Sort files most recent first """
