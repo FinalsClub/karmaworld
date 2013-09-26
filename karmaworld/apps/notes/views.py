82914e9a225a0fbcbb19af0940fd58cd1d783253
Josh Williams <jwilliams@endpoint.com>
2013-09-26 15:27:29 -0400

    Detect and display PDF and PPT note files

--- karmaworld/apps/notes/views.py
+++ karmaworld/apps/notes/views.py
@@ -20,9 +20,7 @@ from karmaworld.apps.notes.forms import NoteForm
 
 
 def is_pdf(self):
-    _path = self.object.note_file.name
-    _, _extension = os.path.splitext(_path)
-    if _extension.lower() == '.pdf':
+    if self.object.file_type == 'pdf':
         return True
     return False
 
@@ -131,7 +129,8 @@ class PDFView(DetailView):
             kwargs['pdf_path'] = "{0}{1}".format(settings.MEDIA_URL,
                 os.path.basename(self.object.pdf_file.name))
         elif is_pdf(self):
-            kwargs['pdf_path'] = "{0}{1}".format(settings.MEDIA_URL,
-                os.path.basename(self.object.note_file.name))
+            kwargs['pdf_path'] = self.object.fp_file
+            #kwargs['pdf_path'] = "{0}{1}".format(settings.MEDIA_URL,
+            #    os.path.basename(self.object.note_file.name))
 
         return super(PDFView, self).get_context_data(**kwargs)
