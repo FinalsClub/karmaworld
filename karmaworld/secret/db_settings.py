ce0b36cbde101e2c68868c645baf06353bc76652
bobcall <bob@bobcall.me>
2013-10-31 08:21:10 -0400

    Added remaining secret templates

---
+++ karmaworld/secret/db_settings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# Copyright (C) 2012  FinalsClub Foundation
+""" DO NOT check this file into source control """
+PROD_DB_NAME = 'DB_NAME'
+PROD_DB_USERNAME = 'DB_USER_NAME'
+PROD_DB_PASSWORD = 'PASSWD'
