0221a17d58a198bc9bcc16e89e25e60d631d82e7
Charles Connell <charles@connells.org>
2013-12-20 19:53:45 -0500

    Zoom buttons on notes

--- karmaworld/assets/js/note-iframe.js
+++ karmaworld/assets/js/note-iframe.js
@@ -1,15 +1,42 @@
-  // resize the iframe based on internal contents on page load
-  function autoResize(id){
-      var newheight;
-      var newwidth;
+// resize the iframe based on internal contents on page load
+function autoResize(id){
+  var newheight;
+  var newwidth;
 
-      if(document.getElementById){
-        newheight = document.getElementById(id).contentWindow.document .body.scrollHeight;
-        newwidth = document.getElementById(id).contentWindow.document .body.scrollWidth;
-      }
-
-      document.getElementById(id).height = (newheight+ 10) + "px";
-      document.getElementById(id).width= (newwidth + 5) + "px";
-      //alert('height: ' + newheight);
+  if(document.getElementById){
+    newheight = document.getElementById(id).contentWindow.document .body.scrollHeight;
+    newwidth = document.getElementById(id).contentWindow.document .body.scrollWidth;
   }
 
+  document.getElementById(id).height = (newheight+ 10) + "px";
+  document.getElementById(id).width= (newwidth + 5) + "px";
+
+
+  var currFFZoom = 1;
+  var currIEZoom = 100;
+  var frameBody = $('#noteframe').contents().find('body');
+
+  $('#plus-btn').on('click',function(){
+    if ($.browser.mozilla){
+      var step = 0.25;
+      currFFZoom += step;
+      frameBody.css('MozTransform','scale(' + currFFZoom + ')');
+    } else {
+      var step = 25;
+      currIEZoom += step;
+      frameBody.css('zoom', ' ' + currIEZoom + '%');
+    }
+  });
+
+  $('#minus-btn').on('click',function(){
+    if ($.browser.mozilla){
+      var step = 0.25;
+      currFFZoom -= step;
+      frameBody.css('MozTransform','scale(' + currFFZoom + ')');
+    } else {
+      var step = 25;
+      currIEZoom -= step;
+      frameBody.css('zoom', ' ' + currIEZoom + '%');
+    }
+  });
+}
