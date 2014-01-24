b3e9fe27f2b0de7adeacab1c9db847e7123c1dd0
Charles Connell <charles@connells.org>
2014-01-23 23:34:21 -0500

    Allow users to decide their username on account creation #303

--- karmaworld/apps/users/forms.py
+++ karmaworld/apps/users/forms.py
@@ -5,11 +5,13 @@ from django import forms
 
 
 class SignupForm(forms.Form):
+    username   = forms.CharField(max_length=255, required=True, label='Username')
     first_name = forms.CharField(max_length=255, required=False, label='Given name')
     last_name  = forms.CharField(max_length=255, required=False, label='Family name')
     email      = forms.EmailField(label='Email address')
 
     def save(self, user):
+        user.username   = self.cleaned_data['username']
         user.first_name = self.cleaned_data['first_name']
         user.last_name  = self.cleaned_data['last_name']
         user.email      = self.cleaned_data['email']
