3d217b85f8a09f31db43055b76b977d165010257
Charles Connell <charles@connells.org>
2014-02-17 12:21:48 -0500

    Improve keyword edit form, particularly for mobile

--- karmaworld/apps/quizzes/forms.py
+++ karmaworld/apps/quizzes/forms.py
@@ -1,17 +1,11 @@
-from django import forms
-from django.forms import TextInput, Textarea
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# Copyright (C) 2014  FinalsClub Foundation
+from django.forms import TextInput, Textarea, HiddenInput, Form, CharField, IntegerField
 
 
-class KeywordForm(forms.Form):
-    keyword = forms.CharField(widget=TextInput)
-    definition = forms.CharField(widget=Textarea)
-    id = forms.IntegerField(required=False)
-
-    def as_p(self):
-        return '<div class="row keyword-form-row"><div class="small-4 columns">' + \
-                self['keyword'].as_text(attrs={'placeholder': 'Keyword', 'class': 'keyword'}) + \
-                '</div><div class="small-8 columns">' + \
-                self['definition'].as_textarea(attrs={'placeholder': 'Definition', 'class': 'definition'}) + \
-                self['id'].as_hidden(attrs={'class': 'object-id'}) + \
-                '</div></div>'
+class KeywordForm(Form):
+    keyword = CharField(widget=TextInput(attrs={'placeholder': 'Keyword', 'class': 'keyword'}))
+    definition = CharField(widget=Textarea(attrs={'placeholder': 'Definition', 'class': 'definition'}))
+    id = IntegerField(widget=HiddenInput(attrs={'class': 'object-id'}), required=False)
 
--- karmaworld/assets/js/quiz.js
+++ karmaworld/assets/js/quiz.js
@@ -1,39 +1,39 @@
 
-function addForm(event) {
-
+function tabHandler(event) {
   // check for:
   // key pressed was TAB
   // key was pressed in last row
   if (event.which == 9 &&
       (!$(this).closest('div.keyword-form-row').next().hasClass('keyword-form-row'))) {
-
-    var prototypeFormString = $('#keyword-form-prototype').text();
-    var newForm = $('#keyword-form-rows').append(prototypeFormString).find('.keyword-form-row:last');
-    var totalForms = $('#id_form-TOTAL_FORMS').attr('value');
-    var newIdRoot = 'id_form-' + totalForms + '-';
-    var newNameRoot = 'form-' + totalForms + '-';
-
-    var keywordInput = newForm.find('.keyword');
-    console.log(newForm);
-    console.log(keywordInput);
-    keywordInput.attr('id', newIdRoot + 'keyword');
-    keywordInput.attr('name', newNameRoot + 'keyword');
-
-    var definitionInput = newForm.find('.definition');
-    definitionInput.attr('id', newIdRoot + 'definition');
-    definitionInput.attr('name', newNameRoot + 'definition');
-    definitionInput.keydown(addForm);
-
-    var objectIdInput = newForm.find('.object-id');
-    objectIdInput.attr('id', newIdRoot + 'id');
-    objectIdInput.attr('name', newNameRoot + 'id');
-
-    $('#id_form-TOTAL_FORMS').attr('value', parseInt(totalForms)+1);
-
+    addForm(event);
   }
 }
 
+function addForm(event) {
+  var prototypeForm = $('#keyword-form-prototype div.keyword-form-row').clone().appendTo('#keyword-form-rows');
+  var newForm = $('.keyword-form-row:last');
+  var totalForms = $('#id_form-TOTAL_FORMS').attr('value');
+  var newIdRoot = 'id_form-' + totalForms + '-';
+  var newNameRoot = 'form-' + totalForms + '-';
+
+  var keywordInput = newForm.find('.keyword');
+  keywordInput.attr('id', newIdRoot + 'keyword');
+  keywordInput.attr('name', newNameRoot + 'keyword');
+
+  var definitionInput = newForm.find('.definition');
+  definitionInput.attr('id', newIdRoot + 'definition');
+  definitionInput.attr('name', newNameRoot + 'definition');
+  definitionInput.keydown(addForm);
+
+  var objectIdInput = newForm.find('.object-id');
+  objectIdInput.attr('id', newIdRoot + 'id');
+  objectIdInput.attr('name', newNameRoot + 'id');
+
+  $('#id_form-TOTAL_FORMS').attr('value', parseInt(totalForms)+1);
+}
+
 $(function() {
-  $('.definition').keydown(addForm);
+  $('.definition').keydown(tabHandler);
+  $('#add-row-btn').click(addForm);
 });
 
