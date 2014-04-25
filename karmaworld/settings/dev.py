f92de2f17f083fe9553e436908b6b17251b89610
Charles Connell <charles@connells.org>
2014-04-25 10:05:15 -0400

    Keeping settings/vmdev.py up to speed

--- karmaworld/settings/dev.py
+++ karmaworld/settings/dev.py
@@ -84,10 +84,6 @@ INTERNAL_IPS = ('127.0.0.1',)
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False # Don't interrput our redirects!
-}
 ########## END TOOLBAR CONFIGURATION
 
 ########## COMPRESSION CONFIGURATION
