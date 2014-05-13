aa30862385d73529351d7419e7af14dbb84130b2
Charles Connell <charles@connells.org>
2014-05-13 14:57:00 -0400

    Fix backup script

--- database_backup.sh
+++ database_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pg_dump -F t karmanotes > karmanotes_db_prod-$(date +"%Y-%m-%d").tar
-glacier-cmd --region us-east-1 upload karmanotes_db_prod-$(date +"%Y-%m-%d").tar
+glacier-cmd --region us-east-1 upload karmanotes_prod_db_backups karmanotes_db_prod-$(date +"%Y-%m-%d").tar
 
