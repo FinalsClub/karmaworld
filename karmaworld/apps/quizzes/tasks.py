e9decf4e0e5bec237696f64010d8c3051a0ded1a
Charles Connell <charles@connells.org>
2014-05-17 18:29:04 -0400

    Change mturk text

--- karmaworld/apps/quizzes/tasks.py
+++ karmaworld/apps/quizzes/tasks.py
@@ -16,21 +16,22 @@ from django.conf import settings
 
 logger = get_task_logger(__name__)
 
-HIT_TITLE = 'Help people learn by finding keywords in college course notes'
+HIT_TITLE_TEMPLATE = 'Get paid to learn {course} at {school}'
 HIT_DESCRIPTION = "Read students' course notes on KarmaNotes.org and " \
                   "identify 10 keywords along with descriptions of them"
 HIT_OVERVIEW_TEMPLATE = \
-        '<p>Go to the page at KarmaNotes.org by clicking the link provided below. ' \
-        '<strong>Identify 10 key words (or short phrases) that are the most important to the document</strong> ' \
-        'on the page. This requires reading and understanding the document. Write these words in the boxes below. ' \
-        'Then, write definitions or descriptions of these keywords as they are provided in the page. ' \
-        'If the page does not provide a definition or description, leave the box blank. ' \
-        'For example, keywords might be &quot;John Locke,&quot; &quot;the Protestant reformation,&quot; ' \
-        'or &quot;existentialism.&quot; Their respective definitions or descriptions might be &quot;life, ' \
-        'liberty, and property,&quot; &quot;schism in Christianity started by Martin Luther,&quot; and ' \
-        '&quot;existence precedes essence.&quot;</p>' \
+        '<p>KarmaNotes.org is a non-profit organization dedicated to free and open education. ' \
+        'We need your help to identify keywords and definitions in college student lecture notes. ' \
+        'Here is one example from an American History course:</p>' \
+        '<p><strong>Constitutional Amendment</strong> &mdash; The process whereby the US ' \
+        'Constitution may be altered by a two-thirds vote of the Senate and House of ' \
+        'Representatives or a vote by at least two-thirds of the states.</p>' \
+        '<p>In the notes below, please find keywords and definitions like the example above.</p>' \
         '<p>Notes link: <strong><a href="http://{domain}{link}">' \
-        'http://{domain}{link}</a></strong></p>'
+        'http://{domain}{link}</a></strong></p>' \
+        '<p>In these notes, please find key words and definitions within these student notes. ' \
+        'With your help, we will generate free and open flashcards and quizzes to help ' \
+        'students study.  Together we can open education, one lecture at a time.</p>'
 HIT_KEYWORDS = 'writing, summary, keywords'
 HIT_DURATION = 60 * 60 * 24 * 7
 HIT_REWARD = 0.92
@@ -77,6 +78,11 @@ def submit_extract_keywords_hit(note):
     connection = MTurkConnection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY,
                                  host=MTURK_HOST)
 
+    if note.course.school:
+        title = HIT_TITLE_TEMPLATE.format(course=note.course.name, school=note.course.school.name)
+    else:
+        title = HIT_TITLE_TEMPLATE.format(course=note.course.name, school=note.course.department.school.name)
+
     overview = Overview()
     overview.append(FormattedContent(HIT_OVERVIEW_TEMPLATE.format(domain=Site.objects.get_current(),
                                                                   link=note.get_absolute_url())))
@@ -108,7 +114,7 @@ def submit_extract_keywords_hit(note):
         question_form.append(definition_question)
 
     connection.create_hit(questions=question_form, max_assignments=1,
-                          title=HIT_TITLE, description=HIT_DESCRIPTION,
+                          title=title, description=HIT_DESCRIPTION,
                           keywords=HIT_KEYWORDS, duration=HIT_DURATION,
                           reward=HIT_REWARD, qualifications=HIT_QUALIFICATION,
                           annotation=str(note.id))
