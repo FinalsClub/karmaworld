6365a208b4e31e99f6266876a8698cc02f4f076b
Charles Connell <charles@connells.org>
2014-05-23 15:06:23 -0400

    Improve celery wrapper script

--- celerywrapper.sh
+++ celerywrapper.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 python manage.py celery worker --pidfile=/tmp/celeryd.pid -l info &
-python manage.py celery beat --pidfile=/tmp/beat.pid -l info &
+sleep 5
+workerpid=`cat /tmp/celeryd.pid`
+echo "Started celery worker with pid $workerpid"
+python manage.py celery beat --pidfile=/tmp/celerybeat.pid -l info &
+sleep 5
+beatpid=`cat /tmp/celerybeat.pid`
+echo "Started celery beat with pid $beatpid"
+wait $workerpid
+wait $beatpid
