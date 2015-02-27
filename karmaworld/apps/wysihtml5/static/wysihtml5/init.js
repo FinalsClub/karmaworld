d22d810664ee19ca30620807e3fd26360a279de4
Bryan <btbonval@gmail.com>
2015-02-26 20:31:53 -0500

    remove debug no-ops and don't call init on load. closes #410

--- karmaworld/apps/wysihtml5/static/wysihtml5/init.js
+++ karmaworld/apps/wysihtml5/static/wysihtml5/init.js
@@ -3,21 +3,5 @@ function initWysihtml5(element) {
     toolbar: element.id + "-toolbar",
     parserRules: wysihtml5ParserRules
   });
-  editor.on("load", function() {
-  });
-  editor.on("change", function() {
-    console.log("EDITOR", editor.value);
-    element.value = editor.value;
-    console.log("ELEMENT", element.value);
-  });
   return editor;
 }
-
-(function() {
-  var elements = document.querySelectorAll("[role='wysihtml5-rich-text']");
-  var elementArr = [];
-  for (var i = 0; i < elements.length; i++) {
-    elementArr.push(elements[i]);
-  }
-  elementArr.forEach(initWysihtml5);
-})();
